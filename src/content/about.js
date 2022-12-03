ff12533e04274caf3ce6e9ee92b7cd1619b553a0
Jose Estrada <webdeveloper@vgloreto.com>
2022-12-02 19:40:37 -0600

    Add about information

--- src/content/about.js
+++ src/content/about.js
@@ -1,12 +1,26 @@
 export const about = [
   {
     title: "Ruby on Rails Web Developer",
-    duration: "Aug 2018 - Sept 2022",
+    duration: "April 2019",
+    company: "VillaGroup",
     icon: "/assets/about/experience.png",
   },
   {
+    title: "Fullstack Web Developer",
+    duration: "May 2017",
+    company: "Oh Marketing",
+    icon: "/assets/about/experience.png",
+  },
+  {
+    title: "BrightCoders Academy",
+    duration: "Dec 20221",
+    company: "",
+    icon: "/assets/about/school.png",
+  },
+  {
     title: "Instituto Tecnologico de Tepic",
-    duration: "Aug 2015 - Sept 2017",
-    icon: "/assets/about/graduation.png",
+    duration: "Aug 2017",
+    company: "",
+    icon: "/assets/about/school.png",
   },
 ];
