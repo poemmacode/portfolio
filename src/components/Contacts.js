807f579ada7593b3ff30a4636bad4ba93e183a52
Jose Estrada <webdeveloper@vgloreto.com>
2022-12-02 19:38:04 -0600

    Add contact email to action form

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -5,7 +5,7 @@ export const Contacts = () => {
     <form
       className={styles.wrapper}
       target="_blank"
-      action="https://formsubmit.co/your@email.com"
+      action="https://formsubmit.co/j.estrada.developer@gmail.com"
       method="POST"
     >
       <div className={styles.inputs}>
