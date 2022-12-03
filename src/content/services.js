216f22d140262a6024cb8af117fbafc2502335a2
Jose Estrada <webdeveloper@vgloreto.com>
2022-12-02 19:40:58 -0600

    Add services information

--- src/content/services.js
+++ src/content/services.js
@@ -2,31 +2,31 @@ export const services = [
   {
     icon: "/assets/services/web-development.png",
     title: "Web Development",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    description: "Programming of highly scalable web projects with the largest standards in the industry, with good practices and fully maintainable code.",
   },
   {
     icon: "/assets/services/seo-optimization.png",
     title: "SEO Optimization",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    description: "Optimization of websites to make it more attractive according to the guidelines of the most popular search engines on the web. We improve the indexability and positioning of your website so that it appears in the first results of google search.",
   },
   {
     icon: "/assets/services/ux-expert.png",
     title: "UI/UX expert",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    description: "A detailed analysis is made to measure the user experience and we establish objectives to improve the way in which the users of your website interact with the contents and elements of it, thus optimizing the calls to action so that your business has better conversions.",
   },
   {
     icon: "/assets/services/cms-solutions.png",
     title: "CMS Solutions",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    description: "We design your content management system in technologies such as Wordpress, creating custom fields and templates tailored to your needs.",
   },
   {
     icon: "/assets/services/cloud-servers.png",
     title: "AWS Cloud Architecture",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    description: "Design of solutions and architectures for all kinds of needs of your web project in amazon web services.",
   },
   {
     icon: "/assets/services/api-desing.png",
     title: "API Design",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    description: "Design, creation and implementation of API in production ready to be consumed by your frontend.",
   },
 ];
